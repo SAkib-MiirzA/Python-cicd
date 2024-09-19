8e309d4fab748944f39a2f0c6404d656b38bd703
SAkib-MiirzA <SAKIB00786@GMAIL.COM>
2024-09-19 17:26:15 +0500

    Python CICD

---
+++ .github/workflows/tests/test_app.py
@@ -0,0 +1,8 @@
+# tests/test_app.py
+
+from src.app import add
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
---
+++ src/app.py
@@ -0,0 +1,7 @@
+# src/app.py
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Add function:", add(1, 2))
